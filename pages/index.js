8a8a8e52fe325aecb1f9ffe386ac49753c1a9661
Sergey Maximyuk <m4ximyuk@yandex.ru>
2022-11-09 14:32:14 +0300

    feat: Load initial cards from array

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,53 @@
-const popup = document.querySelector('.popup');
-const buttonOpenPopup = document.querySelector('.user__edit-button');
-const popupCloseButton = document.querySelector('.popup__close-button');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-buttonOpenPopup.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
-})
+const cardsContainer = document.querySelector('.cards')
 
-popupCloseButton.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
+// Создать карточку
+function createCard(templateSelector, title, link) {
+  const cardTemplate = document.querySelector(templateSelector).content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title')
+
+  cardImage.src = link
+  cardImage.alt = title
+  cardTitle.textContent = title
+
+  return cardElement
+}
+
+// Рендер элемента
+function renderElement(container, element) {
+  container.prepend(element)
+}
+
+
+// Загрузить стартовые карточки из коробки
+initialCards.forEach(card => {
+  renderElement(cardsContainer, createCard('.card-template', card.name, card.link))
 })

9eea0d6363ded248b6e8a6583edbd0b288818512
Sergey Maximyuk <m4ximyuk@yandex.ru>
2022-11-11 16:41:27 +0300

    feat: add delete card function

--- pages/index.js
+++ pages/index.js
@@ -135,14 +135,16 @@ const createCard = (templateSelector, title, link) => {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title')
-  const like = cardElement.querySelector('.card__like-button')
+  const likeButton = cardElement.querySelector('.card__like-button')
+  const deleteButton = cardElement.querySelector('.card__delete-button')
 
   cardImage.src = link
   cardImage.alt = title
   cardTitle.textContent = title
 
   addOpenPopupHandler(cardImage, 'click', cardPopup)
-  addLikeCardHandle(like, 'click')
+  addLikeCardHandle(likeButton, 'click')
+  addDeleteCardHandle(deleteButton, 'click')
 
   return cardElement
 }
@@ -156,6 +158,13 @@ const renderElement = (container, element) => {
 const addLikeCardHandle = (button, action) => {
   button.addEventListener(action, function (evt) {
     evt.target.classList.toggle('card__like-button_active')
+  })
+}
+
+// добавить слушатель удаления карточки
+const addDeleteCardHandle = (button, action) => {
+  button.addEventListener(action, function (evt) {
+    evt.target.closest('.card').remove()
   })
 }
 

ef18ac39059e2c30ffe6321916a1fe02120b5dae
Sergey Maximyuk <m4ximyuk@gmail.com>
2023-03-07 16:34:16 +0300

    feat: add user profile and card load methods

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import {
 	validationConfig,
-	initialCards,
 	cardsContainer,
 	userAvatar,
 	userName,
@@ -23,7 +22,16 @@ import {
 import { enableValidation, resetValidation } from "./validate.js";
 import { createCard, renderElement } from "./card.js";
 import { openPopup, closePopup, openCardPopup } from "./modal.js";
+import {
+	getUserInfo,
+	getInitialCards,
+	updateUserInfo,
+	updateAvatar,
+	loadCard,
+} from "./api.js";
 import "../pages/index.css";
+
+let userId;
 
 const fillProfileForm = () => {
 	userNameField.value = userName.textContent;
@@ -57,48 +65,98 @@ const openEditUserAvatarPopup = () => {
 	openPopup(popupEditUserAvatar);
 };
 
-const submitFormEditProfile = (evt) => {
+const submitFormEditUserInfo = (evt) => {
 	evt.preventDefault();
-	closePopup(popupEditProfile);
-	changeUserInfo();
+	updateUserInfo(userNameField.value, userStatusField.value)
+		.then((res) => {
+			changeUserInfo();
+			closePopup(popupEditProfile);
+		})
+		.catch((err) => {
+			console.log(`Ошибка: ${err}`);
+		});
 };
 
 const submitFormAddCard = (evt) => {
 	evt.preventDefault();
-	closePopup(popupAddCard);
-	renderElement(
-		cardsContainer,
-		createCard(
-			".card-template",
-			".card",
-			cardTitleField.value,
-			cardImageField.value,
-			openCardPopup
-		)
-	);
-	formAddCard.reset();
+	Promise.all([
+		getUserInfo(),
+		loadCard(cardTitleField.value, cardImageField.value),
+	])
+		.then(([user, card]) => {
+			renderElement(
+				cardsContainer,
+				createCard(
+					".card-template",
+					".card",
+					cardTitleField.value,
+					cardImageField.value,
+					openCardPopup,
+					card._id,
+					user._id,
+					card.owner._id,
+					card.likes
+				)
+			);
+			closePopup(popupAddCard);
+			formAddCard.reset();
+		})
+		.catch((err) => {
+			console.log(`Ошибка: ${err}`);
+		});
 };
 
 const submitFormEditUserAvatar = (evt) => {
 	evt.preventDefault();
-	closePopup(popupEditUserAvatar);
-	changeUserAvatar();
+	updateAvatar(userAvatarImageFiled.value)
+		.then((res) => {
+			changeUserAvatar();
+			closePopup(popupEditUserAvatar);
+		})
+		.catch((err) => {
+			console.log(`Ошибка: ${err}`);
+		});
 };
 
 /* - Стартовые карточки - */
-initialCards.forEach((card) => {
-	renderElement(
-		cardsContainer,
-		createCard(".card-template", ".card", card.name, card.link, openCardPopup)
-	);
-});
+const loadAllInfo = () => {
+	Promise.all([getUserInfo(), getInitialCards()])
+		.then(([user, cardsList]) => {
+			userId = user._id;
+			userName.textContent = user.name;
+			userStatus.textContent = user.about;
+			userAvatar.src = user.avatar;
+			cardsList.reverse();
+			cardsList.forEach((card) => {
+				renderElement(
+					cardsContainer,
+					createCard(
+						".card-template",
+						".card",
+						card.name,
+						card.link,
+						openCardPopup,
+						card._id,
+						user._id,
+						card.owner._id,
+						card.likes
+					)
+				);
+			});
+		})
+		.catch((err) => {
+			console.log(`Ошибка: ${err}`);
+		});
+};
+
+loadAllInfo();
 
 /* - Вызов стартовых слушателей - */
 buttonEditProfile.addEventListener("click", openEditProfilePopup);
 buttonAddCard.addEventListener("click", openAddCardPopup);
 buttonEditUserAvatar.addEventListener("click", openEditUserAvatarPopup);
 formAddCard.addEventListener("submit", submitFormAddCard);
-formEditProfile.addEventListener("submit", submitFormEditProfile);
+formEditProfile.addEventListener("submit", submitFormEditUserInfo);
 formEditUserAvatar.addEventListener("submit", submitFormEditUserAvatar);
 
 /* - Валидация - */
